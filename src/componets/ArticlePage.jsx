935a099d47532f793e928c5e49188645793cd3e1
sofiabezertzi <smpezert@me.com>
2022-07-28 14:04:07 +0100

    fixing issues with articlePage

--- src/componets/ArticlePage.jsx
+++ src/componets/ArticlePage.jsx
@@ -5,17 +5,23 @@ import Comments from "./Comments";
 export default function ArticlePage() {
   const { article_id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
-  const [singleArticle, setSingleArticle] = useState([]);
+  const [singleArticle, setSingleArticle] = useState({});
 
   useEffect(() => {
     setIsLoading(true);
 
     fetch(`https://sobe-news.herokuapp.com/api/articles/${article_id}`)
       .then((res) => res.json())
-      .then(({ article }) => setSingleArticle(article));
-    setIsLoading(false);
+      .then((article) => {
+        console.log(article);
+        setSingleArticle(article);
+        setIsLoading(false);
+      });
   }, [article_id]);
 
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <div className="single-article">
       {isLoading && <div>Loading...</div>}
@@ -26,12 +32,12 @@ export default function ArticlePage() {
       <p>{singleArticle.body}</p>
       <ul className="single-articlePage-list">
         <li>
-          created at
+          created at{" "}
           {singleArticle.created_at.slice(0, 10).split("-").reverse().join("-")}
         </li>
         <li>{singleArticle.votes} votes</li>
-        <Comments />
-        {/* <Link to="/comments" /> */}
+        {/* <Comments />
+        <Link to="/comments" /> */}
         <li>{singleArticle.comment_count} comments</li>
       </ul>
     </div>
